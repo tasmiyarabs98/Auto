560efbd60d7362af746e55ba1b04150f15bce5aa
tasmiyarabs98 <107625027+tasmiyarabs98@users.noreply.github.com>
2022-06-16 19:40:53 +0530

    Add files via upload

---
+++ day1.java
@@ -0,0 +1,14 @@
+package p12;
+
+import org.testng.annotations.Test;
+
+public class day1 {
+	@Test
+public void sample() {
+	System.out.println("hello");
+}
+	@Test
+	public void first() {
+		System.out.println("hiii");
+	}
+}
---
+++ day3.java
@@ -0,0 +1,26 @@
+package p12;
+
+import org.testng.annotations.Test;
+
+public class day3 {
+	@Test
+	public void car()
+	{
+		//selenium
+		System.out.println("login car");
+	}
+	
+	@Test
+	public void MobileLogin()
+	{
+		//Appium
+		System.out.println("Mobile login");
+	}
+	
+	@Test
+	public void LoginApI()
+	{
+		//Resr APT automation
+		System.out.println("Login APT");
+	}
+}
